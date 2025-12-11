a5310119c768b275e9b57526a8ac5f7c5595dc3b
Philipp Ruland <p.ruland@bierbaum-proenen.de>
2025-12-11 14:40:11 +0100

    added psadt files and further tasks

---
+++ win-notepad++/files/Invoke-AppDeployToolkit.ps1
@@ -0,0 +1,376 @@
+﻿<#
+
+.SYNOPSIS
+PSAppDeployToolkit - This script performs the installation or uninstallation of Notepad++.
+
+.DESCRIPTION
+- The script is provided as a template to perform an install, uninstall, or repair of an application(s).
+- The script either performs an "Install", "Uninstall", or "Repair" deployment type.
+- The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
+
+The script imports the PSAppDeployToolkit module which contains the logic and functions required to install or uninstall an application.
+
+PSAppDeployToolkit is licensed under the GNU LGPLv3 License - (C) 2025 PSAppDeployToolkit Team (Sean Lillis, Dan Cunningham, Muhammad Mashwani, Mitch Richters, Dan Gough).
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details. You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+.PARAMETER DeploymentType
+The type of deployment to perform.
+
+.PARAMETER DeployMode
+Specifies whether the installation should be run in Interactive (shows dialogs), Silent (no dialogs), or NonInteractive (dialogs without prompts) mode.
+
+NonInteractive mode is automatically set if it is detected that the process is not user interactive.
+
+.PARAMETER AllowRebootPassThru
+Allows the 3010 return code (requires restart) to be passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
+
+.PARAMETER TerminalServerMode
+Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Desktop Session Hosts/Citrix servers.
+
+.PARAMETER DisableLogging
+Disables logging to file for the script.
+
+.EXAMPLE
+powershell.exe -File Invoke-AppDeployToolkit.ps1 -DeployMode Silent
+
+.EXAMPLE
+powershell.exe -File Invoke-AppDeployToolkit.ps1 -AllowRebootPassThru
+
+.EXAMPLE
+powershell.exe -File Invoke-AppDeployToolkit.ps1 -DeploymentType Uninstall
+
+.EXAMPLE
+Invoke-AppDeployToolkit.exe -DeploymentType "Install" -DeployMode "Silent"
+
+.INPUTS
+None. You cannot pipe objects to this script.
+
+.OUTPUTS
+None. This script does not generate any output.
+
+.NOTES
+Toolkit Exit Code Ranges:
+- 60000 - 68999: Reserved for built-in exit codes in Invoke-AppDeployToolkit.ps1, and Invoke-AppDeployToolkit.exe
+- 69000 - 69999: Recommended for user customized exit codes in Invoke-AppDeployToolkit.ps1
+- 70000 - 79999: Recommended for user customized exit codes in PSAppDeployToolkit.Extensions module.
+
+.LINK
+https://psappdeploytoolkit.com
+
+#>
+
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Install', 'Uninstall', 'Repair')]
+    [PSDefaultValue(Help = 'Install', Value = 'Install')]
+    [System.String]$DeploymentType,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Interactive', 'Silent', 'NonInteractive')]
+    [PSDefaultValue(Help = 'Interactive', Value = 'Interactive')]
+    [System.String]$DeployMode,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$AllowRebootPassThru,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$TerminalServerMode,
+
+    [Parameter(Mandatory = $false)]
+    [System.Management.Automation.SwitchParameter]$DisableLogging
+)
+
+
+##================================================
+## MARK: Variables
+##================================================
+
+$adtSession = @{
+    # App variables.
+    AppVendor = 'Notepad++ Team'
+    AppName = 'Notepad++'
+    AppVersion = ''
+    AppArch = ''
+    AppLang = 'EN'
+    AppRevision = '01'
+    AppSuccessExitCodes = @(0)
+    AppRebootExitCodes = @(1641, 3010)
+    AppScriptVersion = '1.0.0'
+    AppScriptDate = '2025-03-08'
+    AppScriptAuthor = 'Jason Bergner'
+
+    # Install Titles (Only set here to override defaults set by the toolkit).
+    InstallName = ''
+    InstallTitle = ''
+
+    # Script variables.
+    DeployAppScriptFriendlyName = $MyInvocation.MyCommand.Name
+    DeployAppScriptVersion = '4.0.6'
+    DeployAppScriptParameters = $PSBoundParameters
+}
+
+function Install-ADTDeployment {
+    ##================================================
+    ## MARK: Pre-Install
+    ##================================================
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
+
+    ## Microsoft Intune Win32 App Workaround - Check If Running 32-bit PowerShell on 64-bit OS, Restart as 64-bit Process
+
+    if (!([Environment]::Is64BitProcess)) {
+        if ([Environment]::Is64BitOperatingSystem) {
+            
+            Write-ADTLogEntry -Message "Running 32-bit PowerShell on 64-bit OS, Restarting as 64-bit Process..." -Severity 1
+            
+            $arguments = "-NoProfile -ExecutionPolicy ByPass -WindowStyle Hidden -File `"" + $myinvocation.mycommand.definition + "`""
+            $path = (Join-Path $Env:SystemRoot -ChildPath "\sysnative\WindowsPowerShell\v1.0\powershell.exe")
+
+            Start-Process $path -ArgumentList $arguments -Wait
+            
+            Write-ADTLogEntry -Message "Finished Running x64 version of PowerShell" -Severity 1
+            Exit
+        }
+        else {
+            Write-ADTLogEntry -Message "Running 32-bit PowerShell on 32-bit OS" -Severity 1
+        }
+    }
+
+    ## Show Welcome Message, close Notepad++ with a 60 second countdown before automatically closing.
+    Show-ADTInstallationWelcome -CloseProcesses notepad++ -CloseProcessesCountdown 60
+
+    ## Show Progress Message (with a message to indicate the application is being uninstalled).
+    Show-ADTInstallationProgress -StatusMessage 'Removing Any Existing Versions of Notepad++. Please Wait...'
+
+    ## Remove Any Existing Versions of Notepad++ (EXE)
+    $appName = Get-ADTApplication -Name 'Notepad++'
+
+    ## Check if Notepad++ application was found
+    if ($appName.Count -gt 0) {
+        ## Extract the installation path from the uninstall string
+        $installPath = $appName.UninstallString -replace '"|uninstall.exe', ''
+    
+        ## Uninstall Notepad++
+        Uninstall-ADTApplication -Name 'Notepad++' -ApplicationType 'EXE' -ArgumentList '/S' -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 5
+
+        ## Remove the Notepad++ installation folder if it exists
+        Remove-ADTFolder -Path "$installPath" -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 5
+    }
+    else {
+        Write-ADTLogEntry -Message "Notepad++ application was not found." -Severity 1
+    } 
+
+    ##================================================
+    ## MARK: Install
+    ##================================================
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+
+    ## Perform Notepad++ Installation
+
+    if ($ENV:PROCESSOR_ARCHITECTURE -eq 'x86') {
+        Write-ADTLogEntry -Message "Detected 32-bit OS Architecture" -Severity 1
+
+        ## Install Notepad++ on 32-bit OS
+        $files = Get-ChildItem -Path "$($adtSession.DirFiles)" -File -Recurse -ErrorAction SilentlyContinue
+
+        $notepadx86 = $files | Where-Object { $_.Name -match 'npp.*\.Installer.exe' }
+        $xml = $files | Where-Object { $_.Name -match 'config.model.xml' }
+        
+        if ($notepadx86.Count -gt 0) {
+            Show-ADTInstallationProgress -StatusMessage 'Installing Notepad++. Please Wait...'
+            Start-ADTProcess -FilePath "$($notepadx86.FullName)" -ArgumentList '/S' -WindowStyle Hidden
+            Start-Sleep -Seconds 5
+        }
+
+        ## Disable Notepad++ Updates
+        if ($xml.Count -gt 0) {
+            Write-ADTLogEntry -Message "Disabling Notepad++ updates with config.model.xml" -Severity 1
+            Copy-ADTFile -Path "$($xml.FullName)" -Destination "$envProgramFiles\Notepad++\" -ErrorAction SilentlyContinue
+            Start-Sleep -Seconds 5
+
+            ## Remove Notepad ++ Updater Folder (If Present)
+            if (Test-Path -Path "$envProgramFiles\Notepad++\updater") {
+                Write-ADTLogEntry -Message "Removing Notepad++ Updater folder" -Severity 1
+                Remove-ADTFolder -Path "$envProgramFiles\Notepad++\updater" -ErrorAction SilentlyContinue
+                Start-Sleep -Seconds 5
+            }        
+        }
+
+        ## Remove Notepad++ Uninstall Start Menu Entry (If Present)
+        if (Test-Path -Path "$envCommonStartMenuPrograms\Notepad++\Uninstall.lnk") {
+            Write-ADTLogEntry -Message "Removing Notepad++ Uninstall Start Menu Entry." -Severity 1
+            Remove-ADTFile -Path "$envCommonStartMenuPrograms\Notepad++\Uninstall.lnk" -ErrorAction SilentlyContinue
+            Start-Sleep -Seconds 5
+        } 
+    }
+    else {
+        Write-ADTLogEntry -Message "Detected 64-bit OS Architecture" -Severity 1
+
+        ## Install Notepad++ on 64-bit OS
+        $files = Get-ChildItem -Path "$($adtSession.DirFiles)" -File -Recurse -ErrorAction SilentlyContinue
+
+        $notepadx64 = $files | Where-Object { $_.Name -match 'npp.*\.Installer.x64.exe' }
+        $xml = $files | Where-Object { $_.Name -match 'config.model.xml' }
+        
+        if ($notepadx64.Count -gt 0) {
+            Show-ADTInstallationProgress -StatusMessage 'Installing Notepad++. Please Wait...'
+            Start-ADTProcess -FilePath "$($notepadx64.FullName)" -ArgumentList '/S' -WindowStyle Hidden
+            Start-Sleep -Seconds 5
+        }
+
+        ## Disable Notepad++ Updates
+        if ($xml.Count -gt 0) {
+            Write-ADTLogEntry -Message "Disabling Notepad++ updates with config.model.xml" -Severity 1
+            Copy-ADTFile -Path "$($xml.FullName)" -Destination "$envProgramFiles\Notepad++\" -ErrorAction SilentlyContinue
+            Start-Sleep -Seconds 5
+
+            ## Remove Notepad ++ Updater Folder (If Present)
+            if (Test-Path -Path "$envProgramFiles\Notepad++\updater") {
+                Write-ADTLogEntry -Message "Removing Notepad++ Updater folder" -Severity 1
+                Remove-ADTFolder -Path "$envProgramFiles\Notepad++\updater" -ErrorAction SilentlyContinue
+                Start-Sleep -Seconds 5
+            }          
+        }
+
+        ## Remove Notepad++ Uninstall Start Menu Entry (If Present)
+        if (Test-Path -Path "$envCommonStartMenuPrograms\Notepad++\Uninstall.lnk") {
+            Write-ADTLogEntry -Message "Removing Notepad++ Uninstall Start Menu Entry." -Severity 1
+            Remove-ADTFile -Path "$envCommonStartMenuPrograms\Notepad++\Uninstall.lnk" -ErrorAction SilentlyContinue
+            Start-Sleep -Seconds 5
+        }         
+    }
+
+    ##================================================
+    ## MARK: Post-Install
+    ##================================================
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
+
+}
+
+function Uninstall-ADTDeployment {
+    ##================================================
+    ## MARK: Pre-Uninstall
+    ##================================================
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
+
+    ## Show Welcome Message, close Notepad++ with a 60 second countdown before automatically closing.
+    Show-ADTInstallationWelcome -CloseProcesses notepad++ -CloseProcessesCountdown 60
+
+    ## Show Progress Message (with a message to indicate the application is being uninstalled).
+    Show-ADTInstallationProgress -StatusMessage 'Uninstalling Any Existing Versions of Notepad++. Please Wait...'
+  
+    ##================================================
+    ## MARK: Uninstall
+    ##================================================
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+
+    ## Uninstall Any Existing Versions of Notepad++
+    $appName = Get-ADTApplication -Name 'Notepad++'
+
+    ## Check if Notepad++ application was found
+    if ($appName.Count -gt 0) {
+        ## Extract the installation path from the uninstall string
+        $installPath = $appName.UninstallString -replace '"|uninstall.exe', ''
+    
+        ## Uninstall Notepad++
+        Uninstall-ADTApplication -Name 'Notepad++' -ApplicationType 'EXE' -ArgumentList '/S' -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 5
+
+        ## Remove the Notepad++ installation folder if it exists
+        Remove-ADTFolder -Path "$installPath" -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 5
+    }
+    else {
+        Write-ADTLogEntry -Message "Notepad++ application was not found." -Severity 1
+    }
+
+    ##================================================
+    ## MARK: Post-Uninstallation
+    ##================================================
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
+
+    ## <Perform Post-Uninstallation tasks here>
+}
+
+function Repair-ADTDeployment {
+    ##================================================
+    ## MARK: Pre-Repair
+    ##================================================
+    $adtSession.InstallPhase = "Pre-$($adtSession.DeploymentType)"
+ 
+    ##================================================
+    ## MARK: Repair
+    ##================================================
+    $adtSession.InstallPhase = $adtSession.DeploymentType
+  
+    ##================================================
+    ## MARK: Post-Repair
+    ##================================================
+    $adtSession.InstallPhase = "Post-$($adtSession.DeploymentType)"
+
+}
+
+
+##================================================
+## MARK: Initialization
+##================================================
+
+# Set strict error handling across entire operation.
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+Set-StrictMode -Version 1
+
+# Import the module and instantiate a new session.
+try {
+    $moduleName = if ([System.IO.File]::Exists("$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1")) {
+        Get-ChildItem -LiteralPath $PSScriptRoot\PSAppDeployToolkit -Recurse -File | Unblock-File -ErrorAction Ignore
+        "$PSScriptRoot\PSAppDeployToolkit\PSAppDeployToolkit.psd1"
+    }
+    else {
+        'PSAppDeployToolkit'
+    }
+    Import-Module -FullyQualifiedName @{ ModuleName = $moduleName; Guid = '8c3c366b-8606-4576-9f2d-4051144f7ca2'; ModuleVersion = '4.0.6' } -Force
+    try {
+        $iadtParams = Get-ADTBoundParametersAndDefaultValues -Invocation $MyInvocation
+        $adtSession = Open-ADTSession -SessionState $ExecutionContext.SessionState @adtSession @iadtParams -PassThru
+    }
+    catch {
+        Remove-Module -Name PSAppDeployToolkit* -Force
+        throw
+    }
+}
+catch {
+    $Host.UI.WriteErrorLine((Out-String -InputObject $_ -Width ([System.Int32]::MaxValue)))
+    exit 60008
+}
+
+
+##================================================
+## MARK: Invocation
+##================================================
+
+try {
+    Get-Item -Path $PSScriptRoot\PSAppDeployToolkit.* | & {
+        process {
+            Get-ChildItem -LiteralPath $_.FullName -Recurse -File | Unblock-File -ErrorAction Ignore
+            Import-Module -Name $_.FullName -Force
+        }
+    }
+    & "$($adtSession.DeploymentType)-ADTDeployment"
+    Close-ADTSession
+}
+catch {
+    Write-ADTLogEntry -Message ($mainErrorMessage = Resolve-ADTErrorRecord -ErrorRecord $_) -Severity 3
+    Show-ADTDialogBox -Text $mainErrorMessage -Icon Stop | Out-Null
+    Close-ADTSession -ExitCode 60001
+}
+finally {
+    Remove-Module -Name PSAppDeployToolkit* -Force
+}
